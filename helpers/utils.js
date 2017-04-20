38ebb3c57f7de67c2352ee47d270681d38abcbac
fearzen3 <ivanhabi2@gmail.com>
2017-04-20 21:56:44 +0700

    rilis2

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,10 +1,29 @@
 var db = require('../models');
+var util = {};
+var models = require('../models/index');
 
-module.exports={
-   isUser : function(input) {
-     db.User.findOne({where:{username : input}})
-     .then(()=>{
-       console.log('ketemu');
-     })
-   }
+
+util.isUser = function(input) {
+  return new Promise((res, rej) => {
+    db.User.findOne({where:{username:input}})
+    .then((user) => {
+      console.log(user);
+      if (user === null) {
+        rej(false)
+      } else {
+        res(user.id)
+      }
+    })
+    .catch(() => {
+      console.log('ada error di db')
+    })
+  })
 }
+
+
+
+
+
+
+
+module.exports = util;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../models');
-var helper = require('../helpers/util');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var input = req.
   res.render('index', { title: 'Express' });
 });
 
